87ae1c724379dc18296f8d1a8d323a1bc0e69201
mayingcong <mayingcong@xingxy.cn>
2016-06-29 08:05:15 +0800

    修改了grunt配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,6 +86,7 @@ module.exports = function(grunt) {
               connect.static('.tmp'),
               connect().use('/css-basis/line-height&vertical-align/bower_components', connect.static('./bower_components')),
               connect().use('/js-basis/jquery/bower_components', connect.static('./bower_components')),
+              connect().use('/js-advanced/jquery/bower_components', connect.static('./bower_components')),
               connect().use('/bower_components', connect.static('./bower_components')),
               connect().use('/fonts', connect.static('<%= config.app %>/bower_components/bootstrap/dist/fonts')),
               connect.static(config.app)
